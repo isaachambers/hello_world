33280be3b07b07764cdd09306120487c04b280b9
Isaac Kiyimba <isaaceduardk@gmail.com>
2020-05-19 01:06:23 +0300

    Basic controll statements and loops

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,4 +1,5 @@
 import com.demo.model.Person
+import java.util.*
 
 fun main(args: Array<String>) {
     println("hello world")
@@ -30,6 +31,65 @@ fun main(args: Array<String>) {
         "Wrong"
     }
     println(wrongAnswer);
+
+    //Kotlin has no switch statement so When Is used.
+
+    when (question.answer) {
+        "34" -> println("Hell No")
+        "23" -> println("Hell Yeah")
+        else -> Unit
+    }
+
+    // Try Catch Block is an expression as well
+
+    val number: Int = try {
+        Integer.parseInt(question.answer)
+    } catch (ex: NumberFormatException) {
+        ex.printStackTrace()
+        -1
+    }
+    println(number + 1);
+
+    // You can make number nullable thei the ? operator
+    val number1: Int? = try {
+        Integer.parseInt(question.answer)
+    } catch (ex: NumberFormatException) {
+        null
+    }
+    println(number1)
+
+
+//     Looping * For loops
+//    Creating a range of values,
+    for (i in 1..10) {
+        println(i + number)
+    }
+
+    println("_")
+    for (i in 1..10 step 3) {
+        println(i + number)
+    }
+
+    for (i in 1 until 23) {
+        println(i + number)
+    }
+
+    var numbers = listOf<Int>(1, 2, 3, 4, 5)
+    for (i in numbers) {
+        println(i)
+    }
+
+    for ((index, element) in numbers.withIndex()) {
+        println("$index contains $element")
+    }
+
+    var data = TreeMap<String, Int>();
+    data["Jame"] = 23
+    data["Mark"] = 55
+
+    for ((name, age) in data) {
+        println("$name $age")
+    }
 
 }
 
